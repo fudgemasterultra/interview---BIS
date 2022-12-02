45b5ca8524955b5eca4229fc529d72ce48ae48ab
fudgemasterultra <79956447+fudgemasterultra@users.noreply.github.com>
2022-12-02 10:19:36 -0500

    Changed for delay
    Changed the manual processes for ending the timer to be automatic after three seconds. Also noticed a bug in the previous if function it wouldn't have allowed the number 1000 to win. Fixed bug.

--- js - test/js/main.js
+++ js - test/js/main.js
@@ -1,24 +1,22 @@
-console.log("loaded in");
-
-//Will need to add an event listener to the button
-let timer = null;
-let number = 0;
-
-document.getElementById("playBtn").addEventListener("click", function () {
-  console.log("clicked");
-  if (timer === null) {
-    timer = setInterval(function () {
-      number = Math.round(Math.random() * 10000);
-      document.getElementById("playerNumber").innerHTML = number;
-    }, 100);
-  } else {
-    clearInterval(timer);
-    timer = null;
-    //Althought it's unlikley to hit zero, we need to check for it.
-    if (number >= 1000 || number <= 0) {
-      alert("To bad! You've lost!");
-    } else {
-      alert("Congratulations! You've won!");
+{
+  let timer = null;
+  let number = 0;
+  document.getElementById("playBtn").addEventListener("click", function () {
+    setTimeout(() => {}, 3000);
+    if (timer === null) {
+      timer = setInterval(function () {
+        number = Math.round(Math.random() * 10000);
+        document.getElementById("playerNumber").innerHTML = number;
+      }, 100);
+      setTimeout(() => {
+        clearInterval(timer);
+        timer = null;
+        if (number > 1000 || number <= 0) {
+          alert("Too bad! You've lost!");
+        } else {
+          alert("You win!");
+        }
+      }, 3000);
     }
-  }
-});
+  });
+}
